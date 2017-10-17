cd09ecd64f10252494051cf007d2966130a49b4b
jvdwijk <joeyvdwijk@gmail.com>
2017-10-17 09:44:34 +0200

    scripts cleaning

---
+++ Assets/Scripts/ScaleObject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class ScaleObject : MonoBehaviour
+{
+	private Material _material;
+	private float _size;
+
+	private void Start()
+	{
+		_material = gameObject.GetComponent<Renderer>().material;
+		_size = 0.1f;
+		ChangeSize();
+	}
+
+	private void Update()
+	{
+		_size = _size - 0.0003f;
+		ChangeSize();
+	}
+    private void OnTriggerStay (Collider other)
+    {
+	    if (other.CompareTag("Player") || !Input.GetKeyDown(KeyCode.Space)) return;
+	    _size = _size + 0.1f;
+	    ChangeSize();
+    }
+
+	private void ChangeSize()
+	{
+		_material.SetFloat("_Amount", _size);
+	}
+}
